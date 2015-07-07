fb7c0bd154dd4fdbe94a26b5611ba1700293a49a
Tom MacWright <tom@macwright.org>
2015-07-07 17:08:51 -0400

    Unify, use files

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var Transform = require('stream').Transform;
 var util = require('util');
 var zlib = require('zlib');
-
-var VectorTile = require('vector-tile').VectorTile;
+var Set = require('es6-set');
+var mapboxVectorTile = require('vector-tile');
 var Protobuf = require('pbf');
-
-var VectorLayerStats = require('./vector_layer_stats');
+var VectorTile = mapboxVectorTile.VectorTile;
+var vectorTileGeometryTypes = mapboxVectorTile.VectorTileFeature.types;
 
 /**
  * TileStatStream is the exported functionality of this module: it is a
@@ -49,6 +49,76 @@ TileStatStream.prototype.getStatistics = function() {
         stats[layer] = this.vectorLayers[layer].getStatistics();
     }
     return stats;
+};
+
+/**
+ * VectorLayer collects statistics from a single vector_layer within
+ * a vector tile.
+ */
+function VectorLayerStats() {
+    this.UNIQUE_VALUES_MAX = 100;
+    this.count = 0;
+    this.fields = {};
+    this.geometryTypeCounts = [0, 0, 0, 0];
+}
+
+VectorLayerStats.prototype.analyzeFeature = function(feature) {
+    this.count++;
+    this.geometryTypeCounts[feature.type]++;
+    for (var name in feature.properties) {
+        this.analyzeProperty(name, feature.properties[name]);
+    }
+};
+
+VectorLayerStats.prototype.analyzeProperty = function(name, value) {
+    if (this.fields[name] === undefined) {
+        this.fields[name] = {
+            min: null,
+            max: null,
+            uniqueValues: new Set()
+        };
+    }
+    var field = this.fields[name];
+
+    if (field.max === null || value > field.max) {
+        field.max = value;
+    }
+
+    if (field.min === null || value < field.min) {
+        field.min = value;
+    }
+
+    if (field.uniqueValues.size < this.UNIQUE_VALUES_MAX &&
+        field.uniqueValues[value] === undefined) {
+        field.uniqueValues.add(value);
+    }
+};
+
+function setToArray(set) {
+    var values = [];
+    set.forEach(function(value) { values.push(value); });
+    return values;
+}
+
+VectorLayerStats.prototype.getStatistics = function() {
+    var fields = {};
+
+    for (var field in this.fields) {
+        fields[field] = {
+            min: this.fields[field].min,
+            max: this.fields[field].max,
+            values: setToArray(this.fields[field].uniqueValues)
+        };
+    }
+
+    return {
+        geometryTypes: this.geometryTypeCounts.reduce(function(memo, count, i) {
+            memo[vectorTileGeometryTypes[i]] = count;
+            return memo;
+        }, {}),
+        count: this.count,
+        fields: fields
+    };
 };
 
 module.exports = TileStatStream;
